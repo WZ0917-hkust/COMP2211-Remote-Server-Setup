71014c9dae60523254c0e3f450481bfb12b13b42
WZ0917-hkust <szhengav@connect.ust.hk>
2025-09-16 11:29:48 +0800

    change download mirror source

--- serverSetup.sh
+++ serverSetup.sh
@@ -4,4 +4,4 @@ sudo apt-get install ./docker-desktop-amd64.deb
 systemctl --user start docker-desktop
 systemctl --user enable docker-desktop
 
-docker run --gpus=all -p 127.0.0.1:9000:8080 us-docker.pkg.dev/colab-images/public/runtime
+docker run --gpus=all -p 127.0.0.1:9000:8080 asia-docker.pkg.dev/colab-images/public/runtime
